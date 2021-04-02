bd0d9d13a8b4ace42473c2b0376d8399f569e282
Stanislav <ilchenko_stas@mail.ru>
2021-04-02 14:21:32 +0300

    Создал ссылку на таблицу, для которой будет создан внешний ключ

--- db/migrate/20210331151355_create_tests.rb
+++ db/migrate/20210331151355_create_tests.rb
@@ -4,7 +4,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
       t.string :title, null: false
       t.integer :level, default: 0, null: false
       t.reference :category, foreign_key: true, null: false
-      t.reference :author, foreign_key: true, null: false
+      t.reference :author, foreign_key: { to_table: :users }, null: false
 
       t.timestamps
     end
