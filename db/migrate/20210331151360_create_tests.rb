809a16ea3a98c416432f91dbc372fbbf07914894
Stanislav <ilchenko_stas@mail.ru>
2021-04-12 21:14:35 +0300

    Создал seed, и методы для Test, User

--- db/migrate/20210331151360_create_tests.rb
+++ db/migrate/20210331151360_create_tests.rb
@@ -3,8 +3,8 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, default: 0, null: false
-      t.reference :category, foreign_key: true, null: false
-      t.reference :author, foreign_key: { to_table: :users }, null: false
+      t.references :category, foreign_key: true, null: false
+      t.references :author, foreign_key: { to_table: :users }, null: false
 
       t.timestamps
     end
