975b945127c2b4b6f3929a1654af550541688fb2
Stanislav <ilchenko_stas@mail.ru>
2021-04-16 14:12:24 +0300

    Изменения в Test, User.Добавление dependent: :destroy

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,15 @@
 class Test < ApplicationRecord
-  belongs_to :category
-  belongs_to :author, class_name: 'User'
-  has_many :questions
-  has_many :results
+  belongs_to :category, optional: true
+  belongs_to :author, class_name: 'User', optional: true
+
+  has_many :questions, dependent: :destroy
+
+  has_many :results, dependent: :destroy
   has_many :users, through: :results
   
   def self.all_titles_by_category(category_name)
-    Test.joins("JOIN categories ON tests.category_id = categories.id")
-        .where("categories.title = :name", name: category_name)
+    Test.joins(:categories)
+        .where(categories: {title: category_name})
         .order(title: :desc)
         .pluck(:title)
   end
