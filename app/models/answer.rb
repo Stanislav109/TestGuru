ad338b9399f9419b7efe6a28b9f4d54ffa706abd
Stanislav <ilchenko_stas@mail.ru>
2021-04-23 20:39:55 +0300

    Изменения в answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_max_answers_amount
+  validate :validate_max_answers_amount, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_max_answers_amount
-    errors.add(:amount) if question.answers.count > 3
+    errors.add(:amount) if question.answers.count >= 4
   end
 end
