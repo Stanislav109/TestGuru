26b0434d0cc921644bee85488ad2d82b22f75df4
Stanislav <ilchenko_stas@mail.ru>
2021-05-08 17:56:25 +0300

    Урок 9. Создал test_passage

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,8 +4,8 @@ class Test < ApplicationRecord
 
   has_many :questions, dependent: :destroy
 
-  has_many :results, dependent: :destroy
-  has_many :users, through: :results
+  has_many :test_passages, dependent: :destroy
+  has_many :users, through: :test_passages
 
   validates :title, presence: true
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
