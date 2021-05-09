cdeb12b0b722f87ec2ac732691d2582711852431
Stanislav <ilchenko_stas@mail.ru>
2021-05-09 10:44:35 +0300

    Изменения в модели test_passage

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,10 +1,11 @@
 class TestPassage < ApplicationRecord
+  SUCCESS_RATIO = 85
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_validation :before_validation_set_next_current_question_id, on: :update
+  before_validation :set_current_question
 
   def completed?
     current_question.nil?
@@ -20,12 +21,13 @@ class TestPassage < ApplicationRecord
   end
 
   def passed?
-    passed_by_percents.to_i >= 85
+    passed_by_percents.to_i >= SUCCESS_RATIO
   end
 
   private
 
   def correct_answer?(answer_ids)
+    return false if answer_ids.nil?
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
@@ -37,11 +39,11 @@ class TestPassage < ApplicationRecord
     test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
-  end
-
-  def before_validation_set_next_current_question_id
-    self.current_question = next_question
+  def set_current_question
+    if new_record?
+      self.current_question = test.questions.first if test.present?
+    else
+      self.current_question = next_question
+    end
   end
 end
