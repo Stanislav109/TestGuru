27f31171912a770ac4f5a69a6be4c5dd2017c7dc
Stanislav <ilchenko_stas@mail.ru>
2021-05-21 19:34:21 +0300

    Lesson11

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,4 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'GitHub', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
-
-  def show_flash_alerts(type)
-    tag.p flash[type], class: "flash #{type}" if flash[type]
-  end
 end
