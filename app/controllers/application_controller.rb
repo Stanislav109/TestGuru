a6c03c88adc4bd51529e79b1e788e6bcacd85b60
Stanislav <ilchenko_stas@mail.ru>
2021-05-24 20:14:54 +0300

    флэш-сообщение

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
     else
       tests_path
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
