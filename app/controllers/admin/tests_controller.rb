94e13d006a546b310da07b56ae2e6d66510f1217
Stanislav <ilchenko_stas@mail.ru>
2021-05-27 08:29:44 +0300

    fix

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -18,7 +18,7 @@ class Admin::TestsController < Admin::BaseController
   def create
     @test = current_user.author.build(test_params)
     if @test.save
-      redirect_to [:admin, @test], notice: t('.success')
+      redirect_to admin_tests_path, notice: t('.success')
     else
       render :new
     end
@@ -26,7 +26,7 @@ class Admin::TestsController < Admin::BaseController
 
   def update
     if @test.update(test_params)
-      redirect_to [:admin, @test], notice: t('.success')
+      redirect_to admin_tests_path, notice: t('.success')
     else
       render :edit
     end
