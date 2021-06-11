a1753cbddea6b9e11b4105e6d6d0979ac8182937
Stanislav <ilchenko_stas@mail.ru>
2021-06-11 14:27:40 +0300

    add password confirmation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 require("utilities/sorting")
-require("utilities/check_password")
+require("utilities/password_confirmation")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
