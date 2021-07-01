2c30bcd75f321d9da8806b533aedb7b5e4ffcb17
Stanislav <ilchenko_stas@mail.ru>
2021-07-01 12:36:55 +0300

    fix

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -6,7 +6,7 @@ class FeedbacksController < ApplicationController
   def create
     body = params[:body]
     user = current_user
-    FeedbacksMailer.feedback_message(body, user).deliver_now
+    #FeedbacksMailer.feedback_message(body, user).deliver_now
     redirect_to new_feedback_path
   end
 end
